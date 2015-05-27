37be9bdbc49582d3e47a17d4135fdd14d2510cff
oleg.strelnikov <oleg.strelnikov@gmail.com>
2015-05-27 15:57:10 +0200

    Added streams: implementor of results collector by mean of std::stream

---
+++ ut_streams.hpp
@@ -0,0 +1,23 @@
+/*
+ * ut_streams.hpp
+ *
+ *  Created on: May 27, 2015
+ *      Author: Oleg Strelnikov
+ */
+
+#ifndef UT_STREAMS_HPP_
+#define UT_STREAMS_HPP_
+
+#include <iostream>
+
+#include "ut.hpp"
+
+#ifndef UT_NAMESPACE
+#define UT_NAMESPACE ut
+#endif
+
+namespace UT_NAMESPACE {
+
+} //namespace
+
+#endif /* UT_STREAMS_HPP_ */
