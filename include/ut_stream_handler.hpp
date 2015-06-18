246fd0efc465123c99eabd790b81cc491a642de3
Oleg Strelnikov <oleg.strelnikov@gmail.com>
2015-06-18 21:09:17 +0300

    Implemented Stream exception handler

--- include/ut_stream_handler.hpp
+++ include/ut_stream_handler.hpp
@@ -8,5 +8,28 @@
 #ifndef INCLUDE_UT_STREAM_HANDLER_HPP_
 #define INCLUDE_UT_STREAM_HANDLER_HPP_
 
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+
+#ifndef UT_NAMESPACE
+#define UT_NAMESPACE ut
+#endif
+
+namespace UT_NAMESPACE {
+
+	template<typename E> class Stream {
+	public:
+		typedef E Exception;
+		template<typename Out> static void Message(Exception const& e, Out o) {
+			std::ostringstream oss;
+			oss << e;
+			std::string s = oss.str();
+			std::copy(std::begin(s), std::end(s), o);
+		}
+	}; //class Stream
+
+} //namespace UT_NAMESPACE
 
 #endif /* INCLUDE_UT_STREAM_HANDLER_HPP_ */
