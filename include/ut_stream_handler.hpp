fb614b43051f9ccf7b5db1e365577271bd3902a2
Oleg Strelnikov <oleg.strelnikov@gmail.com>
2015-06-18 20:29:54 +0300

    Added stream handler

---
+++ include/ut_stream_handler.hpp
@@ -0,0 +1,12 @@
+/*
+ * ut_stream_handler.hpp
+ *
+ *  Created on: Jun 18, 2015
+ *      Author: Oleg Strelnikov
+ */
+
+#ifndef INCLUDE_UT_STREAM_HANDLER_HPP_
+#define INCLUDE_UT_STREAM_HANDLER_HPP_
+
+
+#endif /* INCLUDE_UT_STREAM_HANDLER_HPP_ */
