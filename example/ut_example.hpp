b80fcc04bee3b08e506d8c6fc8fba8a5dc03517a
Oleg Strelnikov <oleg.strelnikov@gmail.com>
2015-06-18 21:14:02 +0300

    Added example

---
+++ example/ut_example.hpp
@@ -0,0 +1,12 @@
+/*
+ * ut_example.hpp
+ *
+ *  Created on: Jun 18, 2015
+ *      Author: ois
+ */
+
+#ifndef EXAMPLE_UT_EXAMPLE_HPP_
+#define EXAMPLE_UT_EXAMPLE_HPP_
+
+
+#endif /* EXAMPLE_UT_EXAMPLE_HPP_ */
