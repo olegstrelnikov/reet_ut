a7c168ec123badc61916561d48f3e8b2f65294d9
oleg.strelnikov <oleg.strelnikov@gmail.com>
2015-07-14 14:34:19 +0200

    Added ut_namespace

---
+++ include/ut_namespace.hpp
@@ -0,0 +1,15 @@
+/*
+ * ut_namespace.hpp
+ *
+ *  Created on: Jul 14, 2015
+ *      Author: Oleg Strelnikov
+ */
+
+#ifndef INCLUDE_UT_NAMESPACE_HPP_
+#define INCLUDE_UT_NAMESPACE_HPP_
+
+#ifndef UT_NAMESPACE
+#define UT_NAMESPACE ut
+#endif
+
+#endif /* INCLUDE_UT_NAMESPACE_HPP_ */
