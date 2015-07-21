15179bcde5b0c0d12354eb82db1080cea3f31855
Oleg Strelnikov <oleg.strelnikov@gmail.com>
2015-07-21 21:18:58 +0300

    Added multidimentionsl arrays support

--- include/ut_equal.hpp
+++ include/ut_equal.hpp
@@ -143,42 +143,67 @@ public:
 	};
 };
 
+// 12-21 - general cases
 template<typename T1, typename T2> bool equal(T1 const& t1, T2 const& t2) {
     return Equal<T1>::template To<T2>::equal(t1, t2);
 }
 
-template<typename T1, typename T2, std::size_t N1> bool equal(T1 const (&t1)[N1], T2 const &t2) {
-    return Sequence<const T1*, T2>::equal(t1, t1 + N1, N1, t2);
-}
-
-template<typename T1, typename T2> bool equal(std::initializer_list<T1> t1, T2 const& t2) {
-    return Sequence<decltype(t1.begin), T2>::equal(t1.begin(), t1.end(), t1.size(), t2);
-}
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// array special cases:
+// 1 - array-array
 template<typename T1, typename T2, std::size_t N1, std::size_t N2> bool equal(T1 const (&t1)[N1], T2 const (&t2)[N2]);
+template<typename T1, typename T2, std::size_t N1, std::size_t N2, std::size_t M1, std::size_t M2>
+	bool equal(T1 const (&t1)[N1][M1], T2 const (&t2)[N2][M2]);
 template<typename T1, typename T2, std::size_t N> bool equal(T1 const (&t1)[N], T2 const (&t2)[N]) {
     return std::equal<T1 const*, T2 const*, typename EqualFunction<T1, T2>::Type>(t1, t1 + N, t2, equal<T1, T2>);
 }
+template<typename T1, typename T2, std::size_t N, std::size_t M> bool equal(T1 const (&t1)[N][M], T2 const (&t2)[N][M]) {
+	for (std::size_t i = 0; i < N; ++i) { //todo: is it possible to replace the loop with std::equal() ?
+		if (!equal(t1[i], t2[i])) {
+			return false;
+		}
+	}
+    return true;
+}
 
+// 2 - array-initializer_list
 template<typename T1, typename T2, std::size_t N1> bool equal(T1 const (&t1)[N1], std::initializer_list<T2> t2) {
     return (N1 == t2.size()) && std::equal<T1 const*, typename std::initializer_list<T2>::const_iterator, typename EqualFunction<T1, T2>::Type>(t1, t1 + N1, t2.begin(), equal<T1, T2>);
 }
 
-template<typename T1, typename T2> bool equal(std::initializer_list<T1> t1, std::initializer_list<T2> t2) {
-    return (t1.size() == t2.size()) && std::equal<typename std::initializer_list<T1>::const_iterator, typename std::initializer_list<T2>::const_iterator, typename EqualFunction<T1, T2>::Type>(t1.begin(), t1.end(), t2.begin(), equal<T1, T2>);
-}
-
-template<typename T1, typename T2, std::size_t N2> bool equal(T1 const& t1, T2 const (&t2)[N2]) {
-    return equal<T2, T1, N2>(t2, t1);
-}
-
+// 2' - initializer_list-array
 template<typename T1, typename T2, std::size_t N2> bool equal(std::initializer_list<T1> t1, T2 const (&t2)[N2]) {
     return equal(t2, t1);
 }
 
+// 3-6 - array-container, string, other
+template<typename T1, typename T2, std::size_t N1> bool equal(T1 const (&t1)[N1], T2 const &t2) {
+    return Sequence<T1 const*, T2>::equal(t1, t1 + N1, N1, t2);
+}
+
+// 3-6'
+template<typename T1, typename T2, std::size_t N2> bool equal(T1 const& t1, T2 const (&t2)[N2]) {
+    return equal<T2, T1, N2>(t2, t1);
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// initializer_list special cases:
+// 7 - initializer_list-initializer_list
+template<typename T1, typename T2> bool equal(std::initializer_list<T1> t1, std::initializer_list<T2> t2) {
+    return (t1.size() == t2.size()) && std::equal<typename std::initializer_list<T1>::const_iterator, typename std::initializer_list<T2>::const_iterator, typename EqualFunction<T1, T2>::Type>(t1.begin(), t1.end(), t2.begin(), equal<T1, T2>);
+}
+
+// 8 - initializer_list-container, string, other
+template<typename T1, typename T2> bool equal(std::initializer_list<T1> t1, T2 const& t2) {
+    return Sequence<decltype(t1.begin), T2>::equal(t1.begin(), t1.end(), t1.size(), t2);
+}
+
+// 8'
 template<typename T1, typename T2> bool equal(T1 const& t1, std::initializer_list<T2> t2) {
     return equal(t2, t1);
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 } //namespace UT_NAMESPACE
 
